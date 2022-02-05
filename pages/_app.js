14d225960cc045aa96ed125c5f246724164e4df4
Mateus Alvarenga <mateus_13wp@hotmail.com>
2022-02-05 10:24:37 -0300

    Squashed commit of the following:
    commit 793c3f5a39a7919980931bdf0b6b8daaa4b6e616
    Author: Mateus Alvarenga <mateus_13wp@hotmail.com>
    Date:   Sat Feb 5 10:23:55 2022 -0300
    
        Login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,11 @@
 import '../styles/global.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <link rel='stylesheet' href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap' />
+      <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
+      <Component {...pageProps} />)
+    </>
+  )
 }
--- pages/index.js
+++ pages/index.js
@@ -2,47 +2,49 @@ import Head from 'next/head'
 import Layout, { siteTitle } from '../components/layout'
 import utilStyles from '../styles/utils.module.css'
 import { getSortedPostsData } from '../lib/posts'
-import Link from 'next/link'
-import Date from '../components/date'
-
-export default function Home({ allPostsData }) {
-  return (
-    <Layout home>
-      <Head>
-        <title>{siteTitle}</title>
-      </Head>
-      <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
-        <p>
-          (This is a sample website - you’ll be building a site like this in{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
-        </p>
-      </section>
-      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
-        <h2 className={utilStyles.headingLg}>Blog</h2>
-        <ul className={utilStyles.list}>
-          {allPostsData.map(({ id, date, title }) => (
-            <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
-            </li>
-          ))}
-        </ul>
-      </section>
-    </Layout>
-  )
-}
+import Button from '@mui/material/Button';
+import ButtonGroup from '@mui/material/ButtonGroup';
+import Box from '@mui/material/Box';
+import GoogleIcon from '@mui/icons-material/Google';
+import GitHubIcon from '@mui/icons-material/GitHub';
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData()
   return {
     props: {
-      allPostsData
+
     }
   }
 }
+export default function Home(props) {
+  return (
+    <Layout >
+
+      <section className={utilStyles.flexCenter}>
+        <h1 className="text-3xl font-bold underline">
+          Login
+        </h1>
+      </section>
+      <section className={utilStyles.headingMd}>
+        <Box className={utilStyles.flexCenter}>
+          <ButtonGroup
+            orientation="vertical"
+            aria-label="vertical contained button group"
+            variant="contained">
+
+            <Button size="medium" color="error" key="one">
+              <GoogleIcon /> Google
+            </Button>
+
+            <Button size="medium" className={utilStyles.bgBlack} key="two" >
+              <GitHubIcon /> Github
+            </Button>
+
+          </ButtonGroup>
+
+        </Box>
+      </section>
+    </Layout>
+  )
+}
+
